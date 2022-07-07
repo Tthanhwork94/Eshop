642cd12142f187e0b298601138eeebb0703af0f4
tanthanh <dtthanh.work@gmail.com>
2022-07-08 01:35:17 +0700

    change database from mssql to mysql

--- eCommerce/src/main/java/com/eCommerce/ECommerceApplication.java
+++ eCommerce/src/main/java/com/eCommerce/ECommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+//change
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 public class ECommerceApplication {
